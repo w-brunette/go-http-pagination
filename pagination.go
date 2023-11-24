fd7c9da8b7f03dc5af5cf9bcf68eef49eb10d299
William <wbrunette@cclindsa.com>
2023-11-24 11:06:05 -0300

    links

--- pagination.go
+++ pagination.go
@@ -13,12 +13,18 @@ type (
 	SortDirection string
 )
 
-type Query struct {
+type QueryPagination struct {
 	Page          int
 	PageSize      int
 	Term          string
 	SortColumn    string
 	SortDirection SortDirection
+	Links         *Links
+}
+
+func (q QueryPagination) OffsetFromPage() int {
+	offset := (q.Page - 1) * q.PageSize
+	return offset
 }
 
 func page(r *http.Request) int {
@@ -42,10 +48,25 @@ func term(r *http.Request) string {
 	return term
 }
 
-func ParseQuery(r *http.Request) Query {
-	return Query{
+func ParseQuery(r *http.Request) QueryPagination {
+	qp := QueryPagination{
 		Page:     page(r),
 		PageSize: pageSize(r),
 		Term:     term(r),
 	}
+
+	currentQuery := r.URL.Query()
+
+	currentQuery.Set("page", strconv.Itoa(qp.Page-1))
+	qp.Links.PrevPage = r.URL.Path + currentQuery.Encode()
+
+	currentQuery.Set("page", strconv.Itoa(qp.Page+1))
+	qp.Links.NextPage = r.URL.Path + currentQuery.Encode()
+	return qp
+}
+
+type Links struct {
+	NextPage string
+	PrevPage string
+	Pages    []string
 }
